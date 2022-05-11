a21de6bdda45a3cff6cfb1c1092480222691aa1e
JuliusA <julius.soltani@gmail.com>
2022-05-11 14:21:19 +0200

    finished the prices page

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     <footer className="grid grid-cols-2 justify-items-center items-center font-bold ">
       <div className="text-xs px-4 md:text-base">
         <p>Questions? Contact me</p>
-        <p>072-3261128</p>
         <p className="text-blue-500">
           <Link href={"/"}>
             <a> salongorchid@gmail.com</a>
@@ -13,7 +12,7 @@ const Footer = () => {
         </p>
       </div>
       <div>
-        <button className="font-bold button m-2 hover:drop-shadow-xl">
+        <button className="font-bold button m-2 hover:bg-fuchsia-300">
           Book Here
         </button>
       </div>
--- pages/prices.js
+++ pages/prices.js
@@ -1,8 +1,52 @@
 const Prices = () => {
   return (
-    <div>
-      <h1 className="font-bold underline text-center pt-20">Prices</h1>
-    </div>
+    <section className="px-2 pt-10 flex justify-around">
+      <div className=" backgroundColor w-12/12 text-sm md:text-base h-auto rounded-xl shadow-2xl ">
+        <div className="pl-3">
+          <h2 className="text-center font-bold pt-1">Bröllop / Wedding</h2>
+          <div className="grid grid-cols-2 justify-items-center">
+            <h3>Bridal Makeup</h3>
+            <div>2000kr</div>
+            <h3>Bridal Hair</h3>
+            <div>2000kr</div>
+            <h3>Bridal Hair & Makeup</h3>
+            <div>3500kr</div>
+          </div>
+        </div>
+        <br />
+        <h2 className="text-center font-bold">Party / Events</h2>
+        <div className="grid grid-cols-2 justify-items-center">
+          <h3>Party Makeup</h3>
+          <div>2000kr</div>
+          <h3>Party Hair</h3>
+          <div>2000kr</div>
+          <h3>Party Hair & Makeup</h3>
+          <div>3500kr</div>
+        </div>
+        <br />
+        <h2 className="text-center font-bold">Theme Event</h2>
+        <div className="grid grid-cols-2 justify-items-center">
+          <h3>Theme Makeup</h3>
+          <div>2000kr</div>
+          <h3>Theme Hair</h3>
+          <div>2000kr</div>
+          <h3>Theme Hair & Makeup</h3>
+          <div>3500kr</div>
+        </div>
+        <br />
+        <div className="flex flex-col">
+          <div className="grid justify-items-center items-center font-bold text-xs ">
+            <p>These prices are the starting prices for the first 2h.</p>
+            <p>Afterwards there is a charge of 100kr/h</p>
+          </div>
+          <div className="px-16 grid justify-items-center">
+            <button className="button font-bold m-2 hover:bg-fuchsia-300 ">
+              Book Here
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 export default Prices;
