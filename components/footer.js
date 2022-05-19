c9552d87cd5eee1acce904cf7f97ef1b4782b102
JuliusA <julius.soltani@gmail.com>
2022-05-19 17:48:16 +0200

    made some changes to the footer

--- components/footer.js
+++ components/footer.js
@@ -4,23 +4,17 @@ import { useState } from "react";
 const Footer = () => {
   const [openModal, setOpenModal] = useState(false);
   return (
-    <footer className="grid grid-cols-2 justify-items-center items-center font-bold ">
-      <div className="text-xs px-4 md:text-base">
-        <p>Questions? Contact me</p>
-        <p className="text-blue-500">
-          <button
-            className="openModalBtn"
-            onClick={() => {
-              setOpenModal(true);
-            }}
-          >
-            Click Here!
-          </button>
-        </p>
+    <footer className="flex items-center justify-center font-bold  ">
+      <div className="text-sm px-4 md:text-base">
+        <button
+          className="openModalBtn "
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          Questions? Contact me
+        </button>
         {openModal && <Modal closeModal={setOpenModal}> </Modal>}
-      </div>
-      <div>
-        <button className="font-bold button">Book Here</button>
       </div>
     </footer>
   );
