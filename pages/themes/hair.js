2048f876b7f65361e97d7d244c867fb3b1649101
JuliusA <julius.soltani@gmail.com>
2022-05-21 08:59:10 +0200

    fixed the images on hair category

--- pages/themes/hair.js
+++ pages/themes/hair.js
@@ -6,46 +6,60 @@ const Hair = () => {
       <section className=" font-bold text-purple-500 text-center px-10">
         <section className="  pb-20 pt-2 ">
           <span className=" text-sm md:text-base lg:text-xl ">
-            Examples of Hairstyles
+            Examples of Hair Styles
           </span>
           <div className=" grid md:grid-cols-2 lg:grid-cols-4 pt-5">
             <figure>
               {" "}
               <Image
-                src="/weddingHair.jpg"
-                alt="#"
-                width={200}
-                height={200}
+                src="/previewHair.jpg"
+                alt="Example of a hair style"
+                width={250}
+                height={400}
+                className="object-cover"
               ></Image>
             </figure>
 
             <figure>
               {" "}
               <Image
-                src="/previewNaglar.jpg"
-                alt="#"
-                width={200}
-                height={200}
+                src="/weddingHair.jpeg"
+                alt="Example of a hair style"
+                width={250}
+                height={400}
+                className="object-cover"
               ></Image>
             </figure>
 
             <figure>
               {" "}
               <Image
-                src="/previewNaglar.jpg"
-                alt="#"
-                width={200}
-                height={200}
+                src="/weddingHairOne.jpg"
+                alt="Example of a hair style"
+                width={250}
+                height={400}
+                className="object-cover"
               ></Image>
             </figure>
 
             <figure>
               {" "}
               <Image
-                src="/previewNaglar.jpg"
-                alt="#"
-                width={200}
-                height={200}
+                src="/weddingHairTwo.jpg"
+                alt="Example of a hair style"
+                width={250}
+                height={400}
+                className="object-cover"
+              ></Image>
+            </figure>
+            <figure>
+              {" "}
+              <Image
+                src="/weddingHairThree.jpg"
+                alt="Example of a hair style"
+                width={250}
+                height={400}
+                className="object-cover"
               ></Image>
             </figure>
           </div>
